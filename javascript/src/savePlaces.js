7c0e92c97f7c2bf4e114dd49e8ab659dacc283e3
Felix <fmedam@jastech-solutions.de>
2019-10-30 13:47:50 +0100

    Implement convertCoordinates function

--- javascript/src/savePlaces.js
+++ javascript/src/savePlaces.js
@@ -1,6 +1,15 @@
 // This is where you implement your solution 
 const convertCoordinates = (agents) => {
-  return [[0,0],[1,2]]
+  if ( !agents || agents.length === 0) {
+    return [];
+  }
+
+  return agents.map((alphaNumber) => {
+    coordinates = [];
+    coordinates[0] = letterToNumber(alphaNumber.slice(0,1));
+    coordinates[1] = parseInt(alphaNumber.slice(1)) - 1;
+    return coordinates;
+  });
 }
 
 const findSafePlaces = (agents) => {
@@ -15,4 +24,13 @@ module.exports = {
   convertCoordinates,
   findSafePlaces,
   adviceForAda
+}
+
+// Helper functions
+const letterPosition = [
+  'A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J'
+]
+
+function letterToNumber(letter) {
+  return letterPosition.findIndex((position) => letter === position);
 }
