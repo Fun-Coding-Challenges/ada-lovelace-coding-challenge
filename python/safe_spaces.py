072737be229303b3e400435daa4e1e27a96adf3f
falco.winkler <falco.winkler@thoughtworks.com>
2019-11-22 19:05:28 +0100

    small refactor

--- python/safe_spaces.py
+++ python/safe_spaces.py
@@ -21,12 +21,13 @@ class SafetyFinder:
         def convert_coordinate(coordinate):
             letter, digit = coordinate[0], coordinate[1:]
             return [ord(letter) - 65, int(digit) - 1]
+
         return [convert_coordinate(coordinate) for coordinate in agents]
 
     def convert_coordinates_inverse(self, agents):
         return [chr(x + 65) + str(y + 1) for x, y in agents]
 
-    def find_safe_spaces(self, agents):
+    def find_safe_spaces(self, agents, l=10):
         """This method will take an array with agent locations and find
         the safest places in the city for Ada to hang out.
 
@@ -37,7 +38,6 @@ class SafetyFinder:
 
         Returns a list of safe spaces in indexed vector form.
         """
-        l = 10
         max_distance = 2 * l
         field = np.full((l, l), max_distance)
         for x, y in agents:
@@ -60,7 +60,7 @@ class SafetyFinder:
                             field[x][y + 1] = min(field[x][y + 1], val + 1)
         return np.argwhere(field == field.max()).tolist()
 
-    def advice_for_ada(self, agents):
+    def advice_for_ada(self, agents, l=10):
         """This method will take an array with agent locations and offer advice
         to Ada for where she should hide out in the city, with special advice for
         edge cases.
@@ -73,9 +73,9 @@ class SafetyFinder:
         or a specialized message informing her of edge cases
         """
         numeric_agents = self.convert_coordinates(agents)
-        save_spaces = self.find_safe_spaces(numeric_agents)
+        save_spaces = self.find_safe_spaces(numeric_agents, l)
         if save_spaces:
-            if len(save_spaces) == 10 * 10:
+            if len(save_spaces) == l ** 2:
                 return 'The whole city is safe for Ada! :-)'
             return self.convert_coordinates_inverse(save_spaces)
         return 'There are no safe locations for Ada! :-('
